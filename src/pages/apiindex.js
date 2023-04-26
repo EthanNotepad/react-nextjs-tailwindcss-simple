42a94466668493dd41828149e51578c95a23f4c8
EthanCheng <svip2011@qq.com>
2023-04-26 19:40:34 +0800

    Added: tailwindcss

--- src/pages/apiindex.js
+++ src/pages/apiindex.js
@@ -3,14 +3,16 @@ import { useState } from "react";
 import styles from "@/styles/ApiIndex.module.css";
 
 async function sendData() {
-  const data = { apikey: "d162ca5", s: "hello", r: "json", page: 2, v: 1 }; // The data you want to send
+  const data = {
+    params: { name: "Sheng", s: "hello", r: "json", page: 2, v: 1 },
+  }; // The data you want to send
 
   const config = {
     headers: { "Content-Type": "application/json" }, // Set the content type to JSON
   };
 
   try {
-    const response = await axios.get("/api/index?name=Query");
+    const response = await axios.get("/api/hello", data, config);
     console.log(response.data);
     return response.data;
   } catch (error) {
@@ -27,8 +29,13 @@ export default function Index() {
   }
 
   return (
-    <div>
-      <button className={styles.button} onClick={handleSubmit}>
+    <div className="container mx-auto p-4">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <p className="text-base pb-8">text-base</p>
+      <button
+        className="font-mono text-lg shadow-sm border-2 rounded-xl border-solid border-indigo-600 px-4 py-2"
+        onClick={handleSubmit}
+      >
         Send Data
       </button>
       {result && <div className={styles.result}>{JSON.stringify(result)}</div>}
